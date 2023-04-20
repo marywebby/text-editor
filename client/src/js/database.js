bdf9250d48dcae4d10694b2634bf3c44640a8641
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-04-19 23:21:25 -0400

    data is saving to the indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  const database = await openDB('jate', 1);
+  const tx = database.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, jate: content  });
+  const result = await request;
+  console.log('🚀  data saved to the database', result);
+  return result;
+};
+;
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    const database = await openDB('jate', 1);
+    const tx = database.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log(result[0]?.jate);
+    return result[0]?.jate;
+};
+
 
 initdb();
